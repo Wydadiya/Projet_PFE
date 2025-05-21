cf41e31198f56088a2e69ca6e0c022dd01169f39
Douae TOUBALI <douaetoubali@gmail.com>
2025-05-21 22:14:04 +0100

    Initial commit - Spring Boot internship project

---
+++ src/main/java/com/pfeProject/Projet_PFE/ProjetPfeApplication.java
@@ -0,0 +1,13 @@
+package com.pfeProject.Projet_PFE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetPfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetPfeApplication.class, args);
+	}
+
+}
